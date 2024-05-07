1dc8f62e9b4a01bf6b249170362cc3e7c1e4fddb
HectorFranco-MISO <h.franco@uniandes.edu.co>
2024-05-07 15:38:08 -0500

    Release

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     baseUrl: 'http://localhost:3000/api/v1',
-    production: true
+    production: false
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    baseUrl: '',
+    baseUrl: 'https://backendcoe.onrender.com/api/v1',
     production: true
 };
